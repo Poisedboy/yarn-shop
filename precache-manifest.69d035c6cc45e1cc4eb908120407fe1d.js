0904e4ff037b1ff47724d3983b7c60de20d912d6
Poisedboy <beschasnuy96@gmail.com>
2020-08-06 10:25:01 +0300

    Updates

---
+++ precache-manifest.69d035c6cc45e1cc4eb908120407fe1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b4c7f7523380eb6d97ae816df0fbae",
+    "url": "/yarn-shop/index.html"
+  },
+  {
+    "revision": "e3b667d6acd1d9dac191",
+    "url": "/yarn-shop/static/css/main.95b479eb.chunk.css"
+  },
+  {
+    "revision": "932ea2ba9d700bd399c2",
+    "url": "/yarn-shop/static/js/2.c4e4b760.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/yarn-shop/static/js/2.c4e4b760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b667d6acd1d9dac191",
+    "url": "/yarn-shop/static/js/main.ce89f79a.chunk.js"
+  },
+  {
+    "revision": "bc4de9bc77c1042d6915",
+    "url": "/yarn-shop/static/js/runtime-main.bcc9b813.js"
+  }
+]);
